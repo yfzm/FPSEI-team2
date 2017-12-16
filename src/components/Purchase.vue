bca07f6435889acc14b710e62c0f5497a3e15b17
byWind <425946578@qq.com>
2017-12-16 21:19:21 +0800

    add Purchase.vue

---
+++ src/components/Purchase.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="purchase">
+        <img id="book_cover" :src="single_item.picture" alt="cover">
+        <p id="book_name">{{ single_item.bookname }}</p>
+        <p id="book_price">价格：{{ single_item.price }}</p>
+        <p id="book_volume">月销量：{{ single_item.sale_volume }}</p>
+        <p class="buyit">
+            <a href="javascript:;" role="button">进入购买页面</a>
+        </p>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Purchase",
+        props: ["single_item"],
+
+
+    }
+</script>
+
+<style>
+    
+</style>
