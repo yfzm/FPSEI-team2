db951fec9e92ebc6dc7dd8d14bd7e8aff1c947dd
yuxinru <1695565257@qq.com>
2017-12-19 16:50:25 +0800

    Add files via upload

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,18 +1,34 @@
 <template>
   <div id="search">
-    <input type="text" placeholder="请输入关键字" id="input_type"/>
+    <input type="text" v-model="my_input" id="input_type">
     <button v-on:click="showNextpage" id="button_type">搜索</button>
   </div>
 </template>
 
 <script>
-//import HelloWorld from './HelloWorld';
+import books from '../assets/data.json'
 export default {
   name: 'Search',
-
+  data: function(){
+    return{
+      search_result:[],
+      input_result:null,
+      my_input:"请输入关键字",
+      right_book : books
+    }
+  },
   methods: {
      showNextpage: function(){
-//       this.$router.go('./HelloWorld')
+       this.search_result=[];
+       this.input_result=this.my_input;
+       let i=0;
+       for(let data in this.right_book.Books){
+         if(this.right_book.Books[i].name===this.input_result){
+           this.search_result.push(this.right_book.Books[i].id);
+         }
+         i++;
+       }
+       this.$emit("sendResult",this.search_result);
      }
   }
 }
@@ -20,26 +36,12 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-#search {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 70px;
-  background: #cccccccc;
-}
-#input_type {
-  position: relative;
-  top: 10px;
-  left: 10px;
+#input_type{
   width: 400px;
   height: 30px;
   font-size:20px;
 }
 #button_type{
-  position:relative;
-  top: 10px;
-  left: 10px;
   width: 70px;
   height:35px;
   font-size:20px;
